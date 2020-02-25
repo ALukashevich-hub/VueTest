b06fcc8bb86758a504723872d3df47288d97c9b2
Andrey L <ALukashevich@tut.by>
2020-02-25 08:35:23 +0300

    :fire: well, first iteration for animation background

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,9 +1,120 @@
 <template>
-  <div id="back"></div>
+  <div id="back">
+    <canvas ref="stage"></canvas>
+  </div>
 </template>
 <script>
+import * as PIXI from 'pixi.js';
+import * as particles from 'pixi-particles';
+import pathRain from '@/assets/HardRain.png';
+
 export default {
   name: 'Background',
+  data() {
+    return {
+      config: {
+        alpha: {
+          start: 1,
+          end: 0,
+        },
+        scale: {
+          start: 0.25,
+          end: 0.1,
+          minimumScaleMultiplier: 1,
+        },
+        color: {
+          start: '#d45d22',
+          end: '#f50a0a',
+        },
+        speed: {
+          start: 400,
+          end: 200,
+          minimumSpeedMultiplier: 1,
+        },
+        acceleration: {
+          x: 40,
+          y: 10,
+        },
+        maxSpeed: 800,
+        startRotation: {
+          min: -140,
+          max: -50,
+        },
+        noRotation: false,
+        rotationSpeed: {
+          min: 0,
+          max: 0,
+        },
+        lifetime: {
+          min: 5,
+          max: 10,
+        },
+        blendMode: 'add',
+        frequency: 0.07,
+        emitterLifetime: -1,
+        maxParticles: 600,
+        pos: {
+          x: 0,
+          y: 0,
+        },
+        addAtBack: false,
+        spawnType: 'rect',
+        spawnRect: {
+          x: -1000,
+          y: 500,
+          w: 1800,
+          h: 60,
+        },
+      },
+    };
+  },
+  mounted() {
+    const canvas = this.$refs.stage;
+    // Basic PIXI Setup
+    const rendererOptions = {
+      view: canvas,
+    };
+    const stage = new PIXI.Container();
+    const renderer = new PIXI.Renderer(rendererOptions);
+    const emitter = new particles.Emitter(
+
+      // The PIXI.Container to put the emitter in
+      // if using blend modes, it's important to put this
+      // on top of a bitmap, and not use the root stage Container
+      stage,
+
+      // The collection of particle images to use
+      [PIXI.Texture.from(pathRain)],
+
+      // Emitter configuration, edit this to change the look
+      // of the emitter
+      this.config,
+
+    );
+    let elapsed = Date.now();
+
+    // Update function every frame
+    const update = () => {
+    // Update the next frame
+      requestAnimationFrame(update);
+
+      const now = Date.now();
+
+      // The emitter requires the elapsed
+      // number of seconds since the last update
+      emitter.update((now - elapsed) * 0.001);
+      elapsed = now;
+
+      // Should re-render the PIXI Stage
+      renderer.render(stage);
+    };
+
+    // Start emitting
+    emitter.emit = true;
+
+    // Start the update
+    update();
+  },
 };
 </script>
 
@@ -17,5 +128,9 @@ export default {
   width: 100%;
   height: 100%;
   z-index: -1;
+}
+canvas {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,10 +1,51 @@
 <template>
-  <canvas id="logo"></canvas>
+<canvas ref="logo" id="logo"></canvas>
 </template>
 
 <script>
+import * as PIXI from 'pixi.js';
+import pathTexture from '@/assets/bunny.png';
+
 export default {
   name: 'Logo',
+  // data() {
+  //   return {
+  //     app: null,
+  //     bunny: null,
+  //     texture: null,
+  //   };
+  // },
+  // mounted() {
+  //   this.app = new PIXI.Application({
+  //     transparent: true,
+  //     view: this.$refs.logo,
+  //   });
+  //   this.texture = PIXI.Texture.from(pathTexture);
+  //   this.bunny = new PIXI.Sprite(this.texture);
+  //   this.bunny.x = this.app.renderer.width / 2;
+  //   this.bunny.y = this.app.renderer.height / 2;
+  //   this.app.stage.addChild(this.bunny);
+  //   this.RunPixi();
+  // },
+  mounted() {
+    const app = new PIXI.Application({
+      transparent: true,
+      view: this.$refs.logo,
+      height: 100,
+      width: 100,
+    });
+    const texture = PIXI.Texture.from(pathTexture);
+    const bunny = new PIXI.Sprite(texture);
+    bunny.anchor.set(0.5);
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+    app.stage.addChild(bunny);
+    app.ticker.add(() => {
+    // just for fun, let's rotate mr rabbit a little
+      bunny.rotation += 0.1;
+    });
+  },
+
 };
 </script>
 
