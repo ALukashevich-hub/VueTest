26aabcfa6cab4752e87c0beb1e14c29bf94a6b2c
Andrey L <Alukashevich@tut.by>
2020-10-15 17:07:32 +0300

    :zap: add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
   devServer: {
     watchOptions: {
       aggregateTimeout: 300,
       poll: 1000,
     },
+  },
+  chainWebpack(config) {
+    config.plugins.delete('prefetch');
+    config.plugin('CompressionPlugin').use(CompressionPlugin);
   },
 };
